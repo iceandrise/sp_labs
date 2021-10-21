9c43109a2d7b50491a66eee13ad4787b364cddae
dominuss-git <kiril6701@gmail.com>
2021-10-21 20:11:06 +0300

    init

---
+++ 8/LR8/SP_LR8.cpp
@@ -0,0 +1,261 @@
+﻿#include "framework.h"
+#include "SP_LR8.h"
+
+#define MAX_LOADSTRING 100
+#define BUTTON_DRAW_ID 1
+#define BUTTON_CLEAR_ID 2
+
+HINSTANCE hInst;                               
+WCHAR szTitle[MAX_LOADSTRING];               
+WCHAR szWindowClass[MAX_LOADSTRING];
+
+bool isVisible = false;
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LR8, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LR8));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LR8));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LR8);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   HWND buttonAdd = CreateWindow(L"BUTTON", NULL, WS_VISIBLE | WS_CHILD | BS_OWNERDRAW | BS_PUSHBUTTON,
+	   20, 320, 60, 60, hWnd, (HMENU)BUTTON_DRAW_ID, NULL, NULL);
+   HWND buttonClear = CreateWindow(L"BUTTON", NULL, WS_VISIBLE | WS_CHILD | BS_OWNERDRAW | BS_PUSHBUTTON,
+	   90, 320, 60, 60, hWnd, (HMENU)BUTTON_CLEAR_ID, NULL, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+			case BUTTON_DRAW_ID:
+				isVisible = true;
+				InvalidateRect(hWnd, NULL, true);
+				break;
+			case BUTTON_CLEAR_ID:
+				isVisible = false;
+				InvalidateRect(hWnd, NULL, true);
+				break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+	case WM_DRAWITEM:
+	{
+		DRAWITEMSTRUCT* draw = (DRAWITEMSTRUCT*)lParam;
+		HBRUSH brush;
+		HPEN pen;
+
+		switch (draw->CtlID)
+		{
+		case BUTTON_DRAW_ID:
+		{
+			HDC hdc = draw->hDC;
+
+			if (draw->itemAction == ODA_SELECT) {
+				brush = CreateSolidBrush(RGB(0, 0, 255));
+			}
+			else {
+				brush = CreateSolidBrush(RGB(0, 255, 0));
+			}
+
+
+			pen = CreatePen(BS_SOLID, 2, RGB(0, 0, 0));
+
+			SelectObject(hdc, brush);
+			SelectObject(hdc, pen);
+
+			Ellipse(hdc, 0, 0, 60, 60);
+			MoveToEx(hdc, 10, 30, NULL);
+			LineTo(hdc, 50, 30);
+			MoveToEx(hdc, 30, 10, NULL);
+			LineTo(hdc, 30, 50);
+		}
+		break;
+		case BUTTON_CLEAR_ID:
+		{
+			HDC hdc = draw->hDC;
+
+			if (draw->itemAction == ODA_SELECT) {
+				brush = CreateSolidBrush(RGB(0, 0, 255));
+			}
+			else {
+				brush = CreateSolidBrush(RGB(255, 0, 0));
+			}
+			pen = CreatePen(BS_SOLID, 2, RGB(0, 0, 0));
+
+			SelectObject(hdc, brush);
+			SelectObject(hdc, pen);
+
+			Ellipse(hdc, 0, 0, 60, 60);
+			MoveToEx(hdc, 20, 20, NULL);
+			LineTo(hdc, 40, 40);
+			MoveToEx(hdc, 40, 20, NULL);
+			LineTo(hdc, 20, 40);
+		}
+		break;
+		default:
+			break;
+		}
+	}
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+			if (isVisible)
+			{
+				HBRUSH brush = CreateSolidBrush(RGB(255, 255, 00));
+				SelectObject(hdc, brush);
+
+                Ellipse(hdc, 30, 30, 300, 300);
+                Rectangle(hdc, 70, 30, 80, 80);
+                Rectangle(hdc, 250, 30, 260, 80);
+
+                brush = CreateSolidBrush(RGB(0, 255, 255));
+                SelectObject(hdc, brush);
+                Ellipse(hdc, 70, 90, 130, 150);
+                Ellipse(hdc, 190, 90, 250, 150);
+
+                POINT points[3];
+                brush = CreateSolidBrush(RGB(0, 0, 0));
+                SelectObject(hdc, brush);
+                MoveToEx(hdc, 80, 250, NULL);                
+                points[0].x = 80;
+                points[0].y = 250;
+                points[1].x = 170;
+                points[1].y = 280;
+                points[2].x = 250;
+                points[2].y = 250;
+                PolyBezierTo(hdc, points, 3);
+
+                MoveToEx(hdc, 80, 250, NULL);
+                points[0].x = 80;
+                points[0].y = 250;
+                points[1].x = 170;
+                points[1].y = 350;
+                points[2].x = 250;
+                points[2].y = 250;
+                PolyBezierTo(hdc, points, 3);
+                
+
+			}
+            EndPaint(hWnd, &ps);
+        }
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+
+    return (INT_PTR)FALSE;
+}
